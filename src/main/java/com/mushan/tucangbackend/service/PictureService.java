1c0a7fe41272d09d04972ee97e137d07896b5784
MuShan <15859445+MoreShame@user.noreply.gitee.com>
2025-08-03 11:59:11 +0800

    图片管理，上传功能

---
+++ src/main/java/com/mushan/tucangbackend/service/PictureService.java
@@ -0,0 +1,89 @@
+package com.mushan.tucangbackend.service;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.mushan.tucangbackend.model.dto.picture.*;
+import com.mushan.tucangbackend.model.entity.Picture;
+import com.mushan.tucangbackend.model.entity.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author Danie
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2025-07-29 15:19:40
+*/
+public interface PictureService extends IService<Picture> {
+
+    /**
+     * 上传图片
+     *
+     * @param inputSource
+     * @param pictureUploadRequest
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(Object inputSource,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser);
+
+    /**
+     * 批量抓取和创建图片
+     *
+     * @param pictureUploadByBatchRequest
+     * @param loginUser
+     * @return 成功创建的图片数
+     */
+    Integer uploadPictureByBatch(
+            PictureUploadByBatchRequest pictureUploadByBatchRequest,
+            User loginUser
+    );
+
+
+
+    /**
+     * 获取查询包装类
+     *
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取图片单条封装类
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+
+    /**
+     * 获取图片分页封装类
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 校验图片
+     * @param picture
+     */
+    void validPicture(Picture picture);
+
+    /**
+     * 图片审核
+     *
+     * @param pictureReviewRequest
+     * @param loginUser
+     */
+    void doPictureReview(PictureReviewRequest pictureReviewRequest, User loginUser);
+
+    void fillReviewParams(Picture picture, User loginUser);
+
+}
--- src/test/java/com/mushan/tucangbackend/TuCangBackendApplicationTests.java
+++ src/test/java/com/mushan/tucangbackend/TuCangBackendApplicationTests.java
@@ -1,10 +1,26 @@
 package com.mushan.tucangbackend;
 
+import com.mushan.tucangbackend.model.entity.User;
+import com.mushan.tucangbackend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class TuCangBackendApplicationTests {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    void batchInsertUsers() {
+
+    }
 
     @Test
     void contextLoads() {
